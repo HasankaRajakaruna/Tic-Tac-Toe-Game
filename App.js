40ae6514177a7f1edd995ae3f4e89f57f7b364d3
Hasanka Rajakaruna <i.h.rajakaruna09.com>
2022-11-27 12:26:48 +0530

    Updated

--- App.js
+++ App.js
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Background from './assets/bg.jpeg';
 
 import {
   ImageBackground,
-  SafeAreaView,
   StyleSheet,
   Text,
   View,
 } from 'react-native';
 
 const App = () => {
+
+
+  const [map,setMap] = useState([
+    ["o","",""],
+    ["","x",""],
+    ["","","x"],
+  ]);
+
   return (
     <View style={styles.container}>
       <ImageBackground source={Background} style={styles.background}>
         <View style={styles.map}>
-          <View style={styles.circle}/>
-          <View>
+          {
+            map.map((row)=>(
+              <View style={styles.row}>
+                {row.map((cell)=>(
+                <View style={styles.cell}>
+                  <View style={styles.circle}/>
+                </View>
+                ))}
+              </View>
+            ))
+          }
+          {/* <View style={styles.circle}/>
+          <View style={styles.cross}>
             <View style={styles.crossLine}></View>
             <View style={[styles.crossLine, styles.crossLineReversed]}></View>
-          </View>
+          </View> */}
         </View>
       </ImageBackground>
     </View>
@@ -40,17 +58,28 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   circle: {
+    // position: 'absolute',
+    // left: 1*100,
+    // right: 1* 100,
     width: 75,
     height: 75,
     borderRadius: 50,
     alignItems: 'center',
     justifyContent: 'center',
     borderColor: 'white',
-    borderWidth: 10
+    borderWidth: 10,
+    margin:10
+  },
+
+  cross:{
+    backgroundColor:'red',
+    width:75,
+    height:75
   },
 
   crossLine: {
     position: 'absolute',
+    left:32.5,
     width: 10,
     height: 75,
     backgroundColor: "white",
@@ -72,6 +101,20 @@ const styles = StyleSheet.create({
     borderColor:"white",
     width: "80%",
     aspectRatio: 1
+  },
+  cell:{
+    width:100,
+    height:100,
+    flex:1,
+    borderColor:'red',
+    borderWidth:1
+  },
+
+  row:{
+    flex:1,
+    flexDirection:'row',
+    // borderColor:'red',
+    // borderWidth: 2,
   }
 
 });
